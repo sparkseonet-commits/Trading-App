206b8d22f4f8952933a47da627a6e15b6d8853c6
sparkseonet-commits <sparkseo.net@gmail.com>
2025-11-08 22:44:36 -0500

    Fix Vite build error by treating App as TSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App.jsx";
+import App from "./app/App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
